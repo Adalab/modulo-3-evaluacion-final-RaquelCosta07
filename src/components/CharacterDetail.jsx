d31dfa4cbbfd7a952b0a14bb38377c7d921021ae
RaquelCosta07 <raquel.s.gomes.costa@gmail.com>
2024-11-29 12:26:45 +0000

    icons added

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -2,27 +2,43 @@
 
 
 function CharacterDetail({ character }) {
-    //Show a loading message or fallback UI if character is undefined
     if (!character) {
         return <p>Loading character details...</p>;
+    }
+
+    // Determine species icon
+    let speciesIcon;
+    if (character.species === "Human") {
+        speciesIcon = "🧑";
+    } else if (character.species === "Alien") {
+        speciesIcon = "👽";
+    }
+
+
+    // Determine status icon
+    let statusIcon;
+    if (character.status === "Alive") {
+        statusIcon = "💚";
+    } else if (character.status === "Dead") {
+        statusIcon = "💀";
+    } else {
+        statusIcon = "❓";
     }
 
     return (
         <div className="card">
             <section className="card-details">
-
                 <img src={character.photo} alt={character.name} />
                 <div className="card-text">
                     <h3>{character.name}</h3>
-                    <p>Species: {character.species}</p>
+                    <p>Species: {speciesIcon} {character.species}</p>
                     <p>Origin: {character.planet}</p>
                     <p>Episodes: {character.episode.length}</p>
-                    <p>Status: {character.status}</p>
+                    <p>Status: {statusIcon} {character.status}</p>
                 </div>
             </section>
         </div>
     );
 }
 
-
-export default CharacterDetail
+export default CharacterDetail;
